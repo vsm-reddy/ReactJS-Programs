62bdd71f538d57f4cec40681ee1b12a0ef869db6
vsm-reddy <suprajamaduryvelagacharla@gmail.com>
2024-07-07 15:09:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import Welcome from "./Welcome" 
+
+function App(){ 
+
+return <Welcome name="pandu"/> 
+
+} 
+
+export default App; 
+
+ 
---
+++ Welcome.js
@@ -0,0 +1,9 @@
+function Welcome(props){ 
+
+ return <h1>{props.name}</h1> 
+    
+} 
+    
+export default Welcome; 
+    
+     
