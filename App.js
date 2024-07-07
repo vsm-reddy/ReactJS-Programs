43cd019947bb15b99565de70bed6685591c378f2
vsm-reddy <suprajamaduryvelagacharla@gmail.com>
2024-07-07 16:21:12 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route,Routes}from 'react-router-dom'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Layout from './Layout'; 
 
-export default App;
+import Home from './Home'; 
+
+import Blogs from './Blogs'; 
+
+import Contact from './Contact'; 
+
+import NoPage from './NoPage'; 
+
+function App(){ 
+ return( 
+
+ <BrowserRouter> 
+
+ <Routes> 
+
+ <Route path="/" element={<Layout/>}> 
+
+ <Route index element={<Home/>}/> 
+
+ <Route path="blogs" element={<Blogs/>}/> 
+
+ <Route path="contact" element={<Contact/>}/> 
+
+ <Route path="'" element={<NoPage/>}/> 
+
+ </Route> 
+
+ </Routes> 
+
+ </BrowserRouter> 
+
+); 
+
+} 
+
+export default App; 
+
+ 
+
---
+++ Blogs.js
@@ -0,0 +1,11 @@
+const Blogs=()=>{ 
+
+     return <h1>Blogs...</h1>; 
+    
+     
+    
+    }; 
+    
+    export default Blogs; 
+    
+     
